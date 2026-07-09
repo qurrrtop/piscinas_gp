e2f9835ada0a2764886dfadcfc8b7fd6af763efb
Mapatipi <Mapatipi@10.0.9.106>
2026-07-09 01:19:02 -0300

    agregue y modifique el dbConnection y productoDAO

--- piscinas_gp/src/main/java/com/mycompany/piscinas_gp/config/DbConnection.java
+++ piscinas_gp/src/main/java/com/mycompany/piscinas_gp/config/DbConnection.java
@@ -1,11 +1,38 @@
 package com.mycompany.piscinas_gp.config;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class DbConnection {
 
-    public Connection getConnection() {
-        throw new UnsupportedOperationException("Not supported yet.");
+// mati con su amigo deam
+public final class DbConnection {
+
+    
+    private static final String URL = "jdbc:mysql://localhost:3306/piscinas_gp";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    private static DbConnection instance;
+
+    //constructor privado para que nadie instancie desde fuera
+    private DbConnection() {
+    }
+
+    //synchronized sirve para que solo un hilo a la vez pueda entrar al método.
+    // es decir si el primer hilo entra en instance == null. crea objeto
+    //al entrar el segundo hilo ve que esta ocupado, espera se desocupa y no crea otro objeto 
+    // es decir para que no haya interferencia de hilos y no se creen muchas instancias o objetos
+    public static synchronized DbConnection getInstance() {
+        if (instance == null) {
+            instance = new DbConnection();
+        }
+
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
     
 }
